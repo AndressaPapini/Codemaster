3fc3484f794eb0e04d49769691f4c6503fd478c3
Andressa Papini <andressa.papini@aluno.senai.br>
2026-03-06 12:14:09 -0300

    Etapa 1 concluída.
    Menu mobile funcional.

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ o menu mobile não abre e o formulário recarrega a página...
 
 _Lista de tarefas 📖_
 
-     [ ] 1. Menu Mobile
+     [✅] 1. Menu Mobile
+     
+O código realizado controla o menu mobile do site, ao clicar no ícone, classes são adicionadas ou removidas para transformar o ícone em X e mostrar ou esconder a lista de links. Enquanto está aberto, o scroll da página é bloqueado. Quando o usuário seleciona um link ou rola a página, tudo volta ao estado inicial e o scroll é liberado novamente.
+
+
      [ ] 2. Links Ativos
      [ ] 3. Formulário com API
      [ ] 4. Animação de Scroll
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// 1ª Digitação (JS Completo)
+// ============ CONTROLE DO MEU MOBILE ================
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navList');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+
+    // Bloquear scroll quando menu aberto
+    document.body.style.overflow = navList.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+    // Fecha menu ao clicar em links 
+    document.querySelectorAll('.navList a').forEach(link => {
+        link.addEventListener('click', () => {
+            menuIcon.classList.remove('bx-x');
+            navList.classList.remove('open');
+            document.body.style.overflow = 'auto';
+        });
+    });
+
+    // Fechar menu ao rolar
+    window.addEventListener('scroll', () => {
+        if (navList.classList.contains ('open')) {
+            menuIcon.classList.remove('bx-x');
+            navList.classList.remove('open');
+            document.body.style.overflow = 'auto';
+        }
+    });
